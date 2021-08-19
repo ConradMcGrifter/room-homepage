ffed7ca0c5d5cf1e6e22f3005ba074761f02f206
skf <conradmcgrifter@protonmail.com>
2021-08-19 18:03:56 -0400

    pretty much done
    I plan on refactoring / organizing the code

--- js/carousel.js
+++ js/carousel.js
@@ -6,6 +6,7 @@ const HAMBURGER_BTN = document.querySelector(".hamburger-btn");
 const CLOSE = document.querySelector(".x");
 const HAMBURGER = document.querySelector(".hamburger");
 const MOBILE_LINKS = document.querySelector(".nav__ul");
+const DARKEN = document.querySelector(".darken");
 
 let slideIndex = 1;
 
@@ -46,4 +47,5 @@ HAMBURGER_BTN.addEventListener("click", () => {
     MOBILE_LINKS.classList.toggle("display");
     CLOSE.classList.toggle("display");
     HAMBURGER.classList.toggle("display");
+    DARKEN.classList.toggle("display");
 });
