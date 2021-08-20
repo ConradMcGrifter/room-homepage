4fc18ae63c2457d2b909ba407a9f5eeb621c6df7
skf <conradmcgrifter@protonmail.com>
2021-08-20 03:21:12 -0400

    added comments to carousel.js
    I also tweaked the js a little bit. I changed the slideIndex from 1 to 0 and adjusted removed the -1 from slideIndex and added it to SLIDES.length

--- js/carousel.js
+++ js/carousel.js
@@ -3,37 +3,45 @@ const INFO = Array.from(document.querySelectorAll(".info--wrap"));
 const PREVIOUS = document.querySelector(".previous--wrap");
 const NEXT = document.querySelector(".next--wrap");
 
-let slideIndex = 1;
+let slideIndex = 0;
 
 const showSlide = (n) => {
-    if (n > SLIDES.length) {
-        slideIndex = 1;
+    // if the number passed into the function is larger than the length of the SLIDES array - 1, reset the slide index to 0 (start)
+    if (n > SLIDES.length - 1) {
+        slideIndex = 0;
     }
 
-    if (n < 1) {
-        slideIndex = SLIDES.length;
+    // if the number passed into the function is smaller than 0, change the slide index to the last slide (whatever the length of the array is - 1)
+    if (n < 0) {
+        slideIndex = SLIDES.length - 1;
     }
 
+    // remove the display class from each slide. this will hide them from view
     SLIDES.forEach((slide) => {
         slide.classList.remove("display");
     });
 
+    // remove the display class from each info-box slide. this will hide them from view
     INFO.forEach((info) => {
         info.classList.remove("display");
     });
 
-    SLIDES[slideIndex - 1].classList.add("display");
-    INFO[slideIndex - 1].classList.add("display");
+    // add the display class to show the specific slide and info-box. [slideIndex] will target the correct element in the array since arrays are 0 based.
+    SLIDES[slideIndex].classList.add("display");
+    INFO[slideIndex].classList.add("display");
 };
 
+// this function is used to icrease or decrease the slideIndex value
 const changeSlide = (n) => {
     showSlide((slideIndex += n));
 };
 
+// left arrow event listener
 PREVIOUS.addEventListener("click", () => {
     changeSlide(-1);
 });
 
+// right arrow event listener
 NEXT.addEventListener("click", () => {
     changeSlide(1);
 });
